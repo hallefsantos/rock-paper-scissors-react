95e58a1a567609d6446fd85c4face3fb28c9e2e9
Hallef Santos <hallefsantos@gmail.com>
2021-07-09 14:48:59 -0300

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,49 @@ const App = () => {
   const [cpuScore, setCpuScore] = useState(0)
 
   useEffect(() => {
-    switch(userChoice + computerChoice) {
-      case 'tesourapapel':
-      case 'pedratesoura':
-      case 'papelpedra':
-        setResult({label: 'You win', bgResult: 'bg-green-500'})
-        setTimeout(() => {
-          setUserScore(s => s+1)
-        }, 750);
-        break
-      case 'papeltesoura':
-      case 'tesourapedra':
-      case 'pedrapapel':
-        setResult({label: 'You lose', bgResult: 'bg-red-500'})
-        setTimeout(() => {
-          setCpuScore(s => s+1)
-        }, 750);
-        break
-      case 'papelpapel':
-      case 'pedrapedra':
-      case 'tesouratesoura':
-        setResult({label: 'Its a draw', bgResult: 'bg-yellow-500'})
-        break
-      default:
-        break
+    if (userChoice) {
+      switch(userChoice.label + computerChoice.label) {
+        case 'tesourapapel':
+        case 'pedratesoura':
+        case 'papelpedra':
+          setResult({label: 'You win', bgResult: 'bg-green-500'})
+          setTimeout(() => {
+            setUserScore(s => s+1)
+          }, 750);
+          break
+        case 'papeltesoura':
+        case 'tesourapedra':
+        case 'pedrapapel':
+          setResult({label: 'You lose', bgResult: 'bg-red-500'})
+          setTimeout(() => {
+            setCpuScore(s => s+1)
+          }, 750);
+          break
+        case 'papelpapel':
+        case 'pedrapedra':
+        case 'tesouratesoura':
+          setResult({label: 'Its a draw', bgResult: 'bg-yellow-500'})
+          break
+        default:
+          break
+      }
     }
   }, [userChoice, computerChoice])
-
-  const choices = ['pedra', 'papel', 'tesoura']
-
-  const choicesEmoji = {
-    pedra: '✊',
-    papel: '✋',
-    tesoura: '✌',
-  }
+  
+  const choicesArr = [
+    {
+      label: 'pedra',
+      emoji: '✊'
+    },
+    {
+      label: 'papel',
+      emoji: '✋'
+    },
+    {
+      label: 'tesoura',
+      emoji: '✌'
+    },
+  ]
 
 
   const handleClick = (value) => {
@@ -56,9 +65,8 @@ const App = () => {
   }
 
   const generateComputerChoice = () => {
-    setComputerChoice(choices[Math.floor(Math.random() * choices.length)])
+    setComputerChoice(choicesArr[Math.floor(Math.random() * choicesArr.length)])
   }
-
 
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-screen bg-gradient-to-r from-gray-100 to-gray-200">
@@ -68,40 +76,39 @@ const App = () => {
         </div>
       )}
       <div className="flex-1 flex items-center text-6xl md:text-7xl">
-        <div className="grid grid-cols-2 text-center gap-5">
-          {isLoading ? (
-            <>
-              <span className="wiggle">✊</span>
-              <span className="wiggle wiggle-reverse">✊</span>
-            </>
-          ) : (
-            <>
-              <span>{choicesEmoji[userChoice]}</span>
-              <span>{choicesEmoji[computerChoice]}</span>
-            </>
-          )}
+        {userChoice && (
+          <div className="grid grid-cols-2 text-center gap-5">
+            {isLoading ? (
+              <>
+                <span className="wiggle">✊</span>
+                <span className="wiggle wiggle-reverse">✊</span>
+              </>
+            ) : (
+              <>
+                <span>{userChoice.emoji}</span>
+                <span>{computerChoice.emoji}</span>
+              </>
+            )}
 
-          {computerChoice && (
-            <>
-              <div className="bg-green-500 text-3xl text-white rounded">YOU</div>
-              <div className="bg-red-500 text-3xl text-white rounded">CPU</div>
-              <div className="col-span-2 grid grid-cols-5">
-                <div className="col-span-2 bg-green-500 text-3xl text-white rounded-l">{userScore}</div>
-                <div className="col-span-1 bg-gray-800 text-3xl text-white">X</div>
-                <div className="col-span-2 bg-red-500 text-3xl text-white rounded-r">{cpuScore}</div>
-              </div>
-            </>
-          )}
-
-
-
-        </div>
-
+            {computerChoice && (
+              <>
+                <div className="bg-green-500 text-3xl text-white rounded">YOU</div>
+                <div className="bg-red-500 text-3xl text-white rounded">CPU</div>
+                <div className="col-span-2 grid grid-cols-5">
+                  <div className="col-span-2 bg-green-500 text-3xl text-white rounded-l">{userScore}</div>
+                  <div className="col-span-1 bg-gray-800 text-3xl text-white">X</div>
+                  <div className="col-span-2 bg-red-500 text-3xl text-white rounded-r">{cpuScore}</div>
+                </div>
+              </>
+            )}
+          </div>
+        )}
       </div>
+
       <div className="mt-10 pb-10 text-7xl md:text-9xl">
-        {choices.map(choice => (
+        {choicesArr.map(choice => (
           <button key={choice} onClick={() => handleClick(choice)} disabled={isLoading}>
-            {choicesEmoji[choice]}
+            {choice.emoji}
           </button>
         ))}
       </div>
